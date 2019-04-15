773976e893ecbeb41122d2789e73b7214c8149d7
Kevin Wang <kw534@cornell.edu>
2019-04-15 14:24:25 -0400

    please help

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -15,4 +15,4 @@ def search():
 	# else:
 	# 	output_message = "Your search: " + query
 	# 	data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message="", data=[]])
+	return render_template('search.html', name=project_name, netid=net_id, output_message="", data=[])
