329878252697d826c4f45fdb61043d3838f64c03
Kevin Wang <kw534@cornell.edu>
2019-04-16 00:16:21 -0400

    lmfao

--- app/templates/search.php
+++ app/templates/search.php
@@ -36,8 +36,8 @@
               </label>
             </div>
             <div id="budget_options" class="custom-select" style="width:200px;">
-              <select>
-                <option value="1">Select a Budget</option>
+              <select name="budgets">
+                <option value="0">Select a Budget</option>
                 <option value="1">$0-$199</option>
                 <option value="2">$200-$399</option>
                 <option value="3">$400-$599</option>
@@ -46,6 +46,85 @@
                 <option value="6">$1000-$1199</option>
                 <option value="7">$1200+</option>
               </select>
+            </div>
+            <div id="feature_options">
+              <table>
+                <tr>
+                  <td>Thinness</td>
+                  <td>Dual SIM</td>
+                  <td>Size</td>
+                  <td>CPU</td>
+                  <td>PPI</td>
+                  <td>SD Slot</td>
+                </tr>
+                <tr>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="thickness">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="sim">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="size">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="cpu">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="ppi">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="card slot">
+                <span class="checkmark"></span>
+              </label></td>
+
+            </tr><tr>
+              <td>Rear Camera</td>
+              <td>Video Qual.</td>
+              <td>AUX</td>
+              <td>Dual Cam</td>
+              <td>Front Cam</td>
+              <td>RAM</td>
+            </tr>
+            <td><label class="container">
+                <input name="feature" type="checkbox" value="rear camera">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="video">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="3.5mm jack">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="dual">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="front camera">
+                <span class="checkmark"></span>
+              </label></td>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="ram">
+                <span class="checkmark"></span>
+              </label></td>
+            </tr>
+            <tr> <td>Internal Storage</td>
+            </tr>
+            <tr>
+              <td><label class="container">
+                <input name="feature" type="checkbox" value="storage">
+                <span class="checkmark"></span>
+              </label></td>
+            </tr>
+          </table>
             </div>
             <script>
             var x, i, j, selElmnt, a, b, c;
@@ -120,272 +199,281 @@
             /*if the user clicks anywhere outside the select box,
             then close all select boxes:*/
             document.addEventListener("click", closeAllSelect);
-          </script>
+            </script>
+            <button id="button_svg" width="100%" height="100%" type="submit" class="button">Click Me</button>
+          </form>
+        </foreignObject>
+      </svg>
+      <svg id ="blue_section" width="0" height="680" x="38%">
+        <rect id="blue_rectangle" x="0" y="0" width="0" height="680"style="fill:#F1F7FF" rx="20" ry="20" />
 
-          <!-- <label style="font-family:Roboto;">
-          <span class="radio">
-          <input class="inputs" type="radio" name="price" value="0">
-          <span class="radio-value" aria-hidden="true"></span>
-        </span>
-        $0-$199
-      </label>
-      <label style="font-family:Roboto;margin-left:5%;">
-      <span class="radio">
-      <input class="inputs" type="radio" name="price" value="200">
-      <span class="radio-value" aria-hidden="true"></span>
-    </span>
-    $200-$399
-  </label>
-  <label style="font-family:Roboto;margin-left:4%;">
-  <span class="radio">
-  <input class="inputs" type="radio" name="price" value="400">
-  <span class="radio-value" aria-hidden="true"></span>
-</span>
-$400-$599
-</label>
-<div id="budget_radios2">
-<label style="font-family:Roboto;">
-<span class="radio">
-<input class="inputs" type="radio" name="price" value="600">
-<span class="radio-value" aria-hidden="true"></span>
-</span>
-$600-$799
-</label>
-<label style="font-family:Roboto;margin-left:5%;">
-<span class="radio">
-<input class="inputs" type="radio" name="price" value="800">
-<span class="radio-value" aria-hidden="true"></span>
-</span>
-$800-$999
-</label>
-<label style="font-family:Roboto;margin-left:4%;">
-<span class="radio">
-<input class="inputs" type="radio" name="price" value="1000">
-<span class="radio-value" aria-hidden="true"></span>
-</span>
-$1000-$1199
-</label>
-</div>
-</div> -->
-<button id="button_svg" width="100%" height="100%" type="submit" class="button">Click Me</button>
-</form>
-</foreignObject>
-</svg>
-<svg id ="blue_section" width="0" height="680" x="38%">
-  <rect id="blue_rectangle" x="0" y="0" width="0" height="680"style="fill:#F1F7FF" rx="20" ry="20" />
+      </svg>
+    </svg>
 
-</svg>
-</svg>
-
-<!-- {% if data %}
-<meta id="my-data" data-playlist="{{ data }}">
-<h1>{{data}}</h1>
-{% endif %} -->
-</div>
-<script>
-let get_request = 0;
-let clicked = 0;
-function setup(){
-  let svg = d3.select("#pink_section")
-  let height = svg.attr("height");
-  let width = svg.attr("width");
+    <!-- {% if data %}
+    <meta id="my-data" data-playlist="{{ data }}">
+    <h1>{{data}}</h1>
+    {% endif %} -->
+  </div>
+  <script>
+  let get_request = 0;
+  let clicked = 0;
+  function setup(){
+    let svg = d3.select("#pink_section")
+    let height = svg.attr("height");
+    let width = svg.attr("width");
 
 
-  svg.append("g")
-  .append("text")
-  .text("sellPhones")
-  .attr("class","svg_text")
-  .attr("font-family", "Roboto")
-  .attr("font-size", "55px")
-  .attr("fill","gray")
-  .attr("text-anchor","middle")
-  .attr("dominantBaseline","middle")
-  .attr("x",width)
-  .attr("y",90);
+    svg.append("g")
+    .append("text")
+    .text("sellPhones")
+    .attr("class","svg_text")
+    .attr("font-family", "Roboto")
+    .attr("font-size", "55px")
+    .attr("fill","gray")
+    .attr("text-anchor","middle")
+    .attr("dominantBaseline","middle")
+    .attr("x",width)
+    .attr("y",90);
 
-  svg.append("g")
-  .append("text")
-  .text("condition")
-  .attr("id","condition")
-  .attr("font-style","italic")
-  .attr("font-family", "Roboto")
-  .attr("font-size","20px")
-  .attr("textLength","86px")
-  .attr("fill","gray")
-  .attr("text-anchor","middle")
-  .attr("dominantBaseline","middle")
-  .attr("x",width)
-  .attr("y",165);
+    svg.append("g")
+    .append("text")
+    .text("condition")
+    .attr("id","condition")
+    .attr("font-style","italic")
+    .attr("font-family", "Roboto")
+    .attr("font-size","20px")
+    .attr("textLength","86px")
+    .attr("fill","gray")
+    .attr("text-anchor","middle")
+    .attr("dominantBaseline","middle")
+    .attr("x",width)
+    .attr("y",165);
 
-  svg.append("g")
-  .append("line")
-  .attr("class","line")
-  .attr("x1","4%")
-  .attr("x2","42%")
-  .attr("y1","160")
-  .attr("y2","160")
-  .attr("stroke","gray")
-  .attr("stroke-linecap","round");
+    svg.append("g")
+    .append("line")
+    .attr("class","line")
+    .attr("x1","4%")
+    .attr("x2","42%")
+    .attr("y1","160")
+    .attr("y2","160")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
 
-  svg.append("g")
-  .append("line")
-  .attr("class","line2")
-  .attr("x1","58%")
-  .attr("x2","96%")
-  .attr("y1","160")
-  .attr("y2","160")
-  .attr("stroke","gray")
-  .attr("stroke-linecap","round");
+    svg.append("g")
+    .append("line")
+    .attr("class","line2")
+    .attr("x1","58%")
+    .attr("x2","96%")
+    .attr("y1","160")
+    .attr("y2","160")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
 
-  svg.append("g")
-  .append("text")
-  .text("budget")
-  .attr("id","budget")
-  .attr("font-style","italic")
-  .attr("font-family", "Roboto")
-  .attr("font-size","20px")
-  .attr("textLength","86px")
-  .attr("fill","gray")
-  .attr("text-anchor","middle")
-  .attr("dominantBaseline","middle")
-  .attr("x",width)
-  .attr("y",250);
+    svg.append("g")
+    .append("text")
+    .text("budget")
+    .attr("id","budget")
+    .attr("font-style","italic")
+    .attr("font-family", "Roboto")
+    .attr("font-size","20px")
+    .attr("fill","gray")
+    .attr("text-anchor","middle")
+    .attr("dominantBaseline","middle")
+    .attr("x",width)
+    .attr("y",250);
 
-  svg.append("g")
-  .append("line")
-  .attr("class","line")
-  .attr("x1","4%")
-  .attr("x2","42%")
-  .attr("y1","245")
-  .attr("y2","245")
-  .attr("stroke","gray")
-  .attr("stroke-linecap","round");
+    svg.append("g")
+    .append("line")
+    .attr("class","line")
+    .attr("x1","4%")
+    .attr("x2","42%")
+    .attr("y1","245")
+    .attr("y2","245")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
 
-  svg.append("g")
-  .append("line")
-  .attr("class","line2")
-  .attr("x1","58%")
-  .attr("x2","96%")
-  .attr("y1","245")
-  .attr("y2","245")
-  .attr("stroke","gray")
-  .attr("stroke-linecap","round");
-}
+    svg.append("g")
+    .append("line")
+    .attr("class","line2")
+    .attr("x1","58%")
+    .attr("x2","96%")
+    .attr("y1","245")
+    .attr("y2","245")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
 
-function fill_blue(){
-  let svg = d3.select("#blue_section")
-  let height = svg.attr("height");
-  let width = svg.attr("width");
+    svg.append("g")
+    .append("text")
+    .text("features")
+    .attr("id","features")
+    .attr("font-style","italic")
+    .attr("font-family", "Roboto")
+    .attr("font-size","20px")
+    .attr("fill","gray")
+    .attr("text-anchor","middle")
+    .attr("dominantBaseline","middle")
+    .attr("x",width)
+    .attr("y",350);
 
-  svg.append("g")
-  .append("text")
-  .text("Search Results")
-  .attr("id","search_results")
-  .attr("font-family", "Roboto")
-  .attr("font-style","italic")
-  .attr("font-size", "30px")
-  .attr("fill","gray")
-  .style("opacity",0)
-  .attr("text-anchor","middle")
-  .attr("dominantBaseline","middle")
-  .attr("x","50%")
-  .attr("y",90);
+    svg.append("g")
+    .append("line")
+    .attr("class","line")
+    .attr("x1","4%")
+    .attr("x2","42%")
+    .attr("y1","345")
+    .attr("y2","345")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
 
-  d3.select("#search_results")
-  .transition()
-  .style("opacity",1)
-  .delay(100);
-
-  svg.append("g")
-  .append("line")
-  .attr("class","line")
-  .attr("x1","4%")
-  .attr("x2","96%")
-  .attr("y1","110")
-  .attr("y2","110")
-  .attr("stroke","gray")
-  .attr("stroke-linecap","round");
-}
-
-function move_svg(){
-
-  d3.select(".line2")
-  .transition()
-  .duration(300)
-  .attr("x1","60%");
-
-  d3.select(".line")
-  .transition()
-  .duration(300)
-  .attr("x2","40%");
-
-  d3.select("#pink_section")
-  .transition()
-  .duration(300)
-  .attr("width", "35%")
-  .attr("x","0");
-
-  d3.select("#blue_section")
-  .transition()
-  .duration(300)
-  .attr("width", "62%");
-
-  d3.select("#blue_rectangle")
-  .transition()
-  .duration(300)
-  .attr("width", "100%");
-
-  d3.select("#condition_radio")
-  .transition()
-  .duration(300)
-  .attr("x", "15%");
-
-  clicked = 1;
-  setTimeout(function() { fill_blue(); }, 300);
-  return false;
-
-}
-
-function fast_move_svg(){
-
-  d3.select(".line2")
-  .attr("x1","60%");
-
-  d3.select(".line")
-  .attr("x2","40%");
-
-  d3.select("#pink_section")
-  .attr("width", "35%")
-  .attr("x","0");
-
-  d3.select("#blue_section")
-  .attr("width", "62%");
-
-  d3.select("#blue_rectangle")
-  .attr("width", "100%");
-
-  d3.select("#condition_radio")
-  .attr("x", "30%");
-
-  clicked = 1;
-  setTimeout(function() { fill_blue(); }, 300);
-  return false;
-
-}
-if("{{check}}" === "True" && "{{flag}}" === "True"){
-  setup();
-  fast_move_svg();
-}
-else if ("{{check}}" === "True"){
-  setup();
-  move_svg();
-}else{
-  setup();
-  if("{{condition}}" == "new" || "{{condition}}" == "old" ){
-    console.log("{{condition}}");
-    move_svg();
+    svg.append("g")
+    .append("line")
+    .attr("class","line2")
+    .attr("x1","58%")
+    .attr("x2","96%")
+    .attr("y1","345")
+    .attr("y2","345")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
   }
-}
+
+  function fill_blue(){
+    let svg = d3.select("#blue_section")
+    let height = svg.attr("height");
+    let width = svg.attr("width");
+
+    svg.append("g")
+    .append("text")
+    .text("Search Results")
+    .attr("id","search_results")
+    .attr("font-family", "Roboto")
+    .attr("font-style","italic")
+    .attr("font-size", "30px")
+    .attr("fill","gray")
+    .style("opacity",0)
+    .attr("text-anchor","middle")
+    .attr("dominantBaseline","middle")
+    .attr("x","50%")
+    .attr("y",90);
+
+    d3.select("#search_results")
+    .transition()
+    .style("opacity",1)
+    .delay(100);
+
+    svg.append("g")
+    .append("line")
+    .attr("class","line")
+    .attr("x1","4%")
+    .attr("x2","96%")
+    .attr("y1","110")
+    .attr("y2","110")
+    .attr("stroke","gray")
+    .attr("stroke-linecap","round");
+  }
+
+  function move_svg(){
+
+    d3.select(".line2")
+    .transition()
+    .duration(300)
+    .attr("x1","60%");
+
+    d3.select(".line")
+    .transition()
+    .duration(300)
+    .attr("x2","40%");
+
+    d3.select("#pink_section")
+    .transition()
+    .duration(300)
+    .attr("width", "35%")
+    .attr("x","0");
+
+    d3.select("#blue_section")
+    .transition()
+    .duration(300)
+    .attr("width", "62%");
+
+    d3.select("#blue_rectangle")
+    .transition()
+    .duration(300)
+    .attr("width", "100%");
+
+    d3.select("#condition_radio")
+    .transition()
+    .duration(300)
+    .attr("x", "15%");
+
+    names = {{names | tojson}};
+    urls = {{urls | tojson}};
+
+    if(names.length > 0){
+      for(let i = 0; i < 6; i++){
+        d3.select("#blue_section").append('text')
+          .text(i+1 + ") " + names[i])
+          .attr("font-family", "Roboto")
+          .attr("font-size", "20px")
+          .attr("fill","gray")
+          .attr("x",i%3*30+5 + "%")
+          .attr("y",Math.floor((i/3))%2*40+20 + "%");
+
+        d3.select("#blue_section").append('image')
+          .attr('xlink:href', urls[i])
+          .attr('width', 200)
+          .attr('height', 200)
+          .attr('x', i%3*30+10 + "%")
+          .attr('y', Math.floor((i/3))%2*40+25 + "%");
+      }
+    }
+
+    clicked = 1;
+    setTimeout(function() { fill_blue(); }, 300);
+    return false;
+
+  }
+
+  function fast_move_svg(){
+
+    d3.select(".line2")
+    .attr("x1","60%");
+
+    d3.select(".line")
+    .attr("x2","40%");
+
+    d3.select("#pink_section")
+    .attr("width", "35%")
+    .attr("x","0");
+
+    d3.select("#blue_section")
+    .attr("width", "62%");
+
+    d3.select("#blue_rectangle")
+    .attr("width", "100%");
+
+    d3.select("#condition_radio")
+    .attr("x", "15%");
+
+    clicked = 1;
+    setTimeout(function() { fill_blue(); }, 300);
+    return false;
+
+  }
+  if("{{check}}" === "True" && "{{flag}}" === "True"){
+    setup();
+    fast_move_svg();
+  }
+  else if ("{{check}}" === "True"){
+    setup();
+    move_svg();
+  }else{
+    setup();
+    if("{{condition}}" == "new" || "{{condition}}" == "old" ){
+      console.log("{{condition}}");
+      move_svg();
+    }
+  }
+
 
 
 
